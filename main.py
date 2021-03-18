277bcfb2e8ff0b3470f55e073c7468e7b093ea14
Stasy <55661709+stasykrylova@users.noreply.github.com>
2021-03-18 12:37:47 +0300

    Add files via upload
    removed global variables, added a pre- transition to a new screen with the game

--- main.py
+++ main.py
@@ -4,50 +4,39 @@ from kivy.lang import Builder
 from kivy.core.audio import SoundLoader
 import time
 
-name=''
 
 class MainWindow(Screen):
     sound = SoundLoader.load("meeting (online-audio-converter.com).wav")
     sound.play()
 
-
     def change_screen(self):
-        print("oki")
-        vol=1
 
-        for i in range(0,20):
-            self.sound.volume=vol
+        vol = 1
+
+        for i in range(0, 20):
+            self.sound.volume = vol
             time.sleep(0.05)
-            vol-=0.05
+            vol -= 0.05
         self.sound.stop()
 
 
 class ThirdWindow(Screen):
-    global name
-    player_name = name
-    def getName(self):
-        print(self.gamer_name.text)
-        self.gamer_name.text =self.gamer_name.text+ name
 
-
+    def play(self):
+        pass
 
 
 class SecondWindow(Screen):
-
-
-    def save_name(self):
-        global name
-        if self.input_name.text=="":
-            name="Друзь"
-        else:
-            name=self.input_name.text
-
-
-
-class WindowManager(ScreenManager):
     pass
 
+
+class Manager(ScreenManager):
+    pass
+
+
 kv = Builder.load_file("my.kv")
+
+
 class MainApp(App):
     def build(self):
         return kv
